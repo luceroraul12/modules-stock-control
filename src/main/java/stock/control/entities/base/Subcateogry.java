2d71293f481e86b546e6ebf66b5feef0bd35f00d
Lucero Raul <luceroraul12@gmail.com>
2022-12-05 16:39:16 -0300

    add category and subcategory

---
+++ src/main/java/stock/control/entities/base/Subcateogry.java
@@ -0,0 +1,10 @@
+package stock.control.entities.base;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Subcateogry extends Identificable {
+    private String name;
+}
