d935cea334cca3834ec06dd38cf1d8307e8da945
Lucero Raul <luceroraul12@gmail.com>
2022-12-05 09:21:44 -0300

    getter, setters and @Annotations

--- src/main/java/stock/control/entities/Product.java
+++ src/main/java/stock/control/entities/Product.java
@@ -1,9 +1,40 @@
 package stock.control.entities;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
     private Long id;
+
     private LocalDateTime dateCreated;
     private LocalDateTime dateModified;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return this.dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public LocalDateTime getDateModified() {
+        return this.dateModified;
+    }
+
+    public void setDateModified(LocalDateTime dateModified) {
+        this.dateModified = dateModified;
+    }
 }
