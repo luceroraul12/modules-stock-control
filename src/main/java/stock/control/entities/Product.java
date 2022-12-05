95d3445e9369cd2cd0d1fcbab23b91143b4064a3
Lucero Raul <luceroraul12@gmail.com>
2022-12-05 09:10:03 -0300

    add product entity

---
+++ src/main/java/stock/control/entities/Product.java
@@ -0,0 +1,9 @@
+package stock.control.entities;
+
+import java.time.LocalDateTime;
+
+public class Product {
+    private Long id;
+    private LocalDateTime dateCreated;
+    private LocalDateTime dateModified;
+}
